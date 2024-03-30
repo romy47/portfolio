1932901fb9923c677a9ae4a3fe8bb402b43ce8af
Sebastian <romy6047@gmail.com>
2024-03-30 16:09:25 -0600

    updated project name

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -55,7 +55,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
