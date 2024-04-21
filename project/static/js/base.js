4b4984874272a01c4abca28d04d40221ad492f06
Sebastian <romy6047@gmail.com>
2024-04-20 21:46:29 -0600

    slider added for homepage projects section

---
+++ project/static/js/base.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        minSlides: 1,
+        maxSlides: 3,
+        slideWidth: 500,
+        controls: true,
+        responsive: true,
+        auto: true,
+        shrinkItems: true,
+        moveSlides: 1
+    });
+});
