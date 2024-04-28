6471465b5c05c3a63be245ab0d444325bf091285
Sebastian <romy6047@gmail.com>
2024-04-27 21:17:00 -0600

    DB switched to postgres

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -3,3 +3,13 @@ from .settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'sebastian123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- project/settings/prod.py
+++ project/settings/prod.py
@@ -3,3 +3,13 @@ from .settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['54.163.42.137']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'himu',
+        'PASSWORD': 'unhcevg6.4cec2456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
