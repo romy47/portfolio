213477748df51f99ac9b1024662d9eba1127eb53
Sebastian <romy6047@gmail.com>
2024-09-15 12:26:17 -0600

    CI CD with github actions

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
